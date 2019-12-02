854235d809190755aff91286fd29041b6de25860
Tyrik Gibbs <tyrikgibbs@outlook.com>
2019-12-02 06:10:32 +0000

    Done.

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -10,3 +10,11 @@ def happy_birthday(birthday_kids)
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
   end
 end
+
+
+def age_appropriate_birthday(birthday_kids)
+  # add your code snippet here!
+  birthday_kids.each do |kids_name, age|
+    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+  end
+end
